b5d43a5cb348c111d866e10719e65c3bfb4a73b9
Shawn Schwartz <stschwartz@stanford.edu>
2022-01-18 01:11:02 -0800

    finished infrastructure for both viewmodel and model

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -4,11 +4,29 @@
 //
 //  Created by Shawn Schwartz on 1/17/22.
 //
-// View Model
+// ViewModel
 //
 
 import SwiftUI
 
+// Notes:
+//  ViewModel will create it's own model ->
+//    the ViewModel itself "is the truth of the app"
+//    (i.e., once it's gone, the app/game is over)
+
 class EmojiMemoryGame {
+    static let emojis = ["🐶", "🐱",  "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐷", "🐸", "🐵", "🐔", "🐧", "🐤", "🐣", "🦆", "🦉", "🐴", "🦄", "🦋", "🐠"]
     
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private(set) var model: MemoryGame<String> = createMemoryGame() // private(set) -> other classes and structs can look at the model but they can't change it
+ 
+    // could use this as a "read-only" method for a private model var (instead of private(set)):
+    // var cards: Array<MemoryGame<String>.Card> {
+    //    return model.cards
+    // }
 }
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -10,15 +10,26 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
     func choose(_ card: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        // add numberOfPairsOfCards x 2 to cards array (to get the pairs)
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent // better to use a "don't care" type here, instead of a String, for flexibility in the future
     }
 }
